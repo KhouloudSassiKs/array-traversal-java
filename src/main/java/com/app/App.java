c0303d52af1c641091743384f0e58e935acc246f
Khouloud <sassikhouloud45@gmail.com>
2025-08-25 21:35:45 +0200

    Update App.java

--- src/main/java/com/app/App.java
+++ src/main/java/com/app/App.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class App {
 
     public static void main(String[] args) {
+        // Example 1: Spiral Traversal
         char[][] grid = {
             {'a','b','c'},
             {'d','e','f'},
@@ -14,5 +15,16 @@ public class App {
         SpiralTraversal spiral = new SpiralTraversal();
         int[] vowels = spiral.spiralTraverseAndVowels(grid);
         System.out.println("Vowel indices in spiral order: " + Arrays.toString(vowels));
+
+        // Example 2: Cross Traversal / Search
+        char[][] board = {
+            {'*','M','*','*'},
+            {'M','*','M','*'},
+            {'*','M','*','*'},
+            {'*','*','*','*'}
+        };
+
+        int crossCount = CrossTraversal.countSubmatricesWithM(board);
+        System.out.println("Number of cross patterns: " + crossCount);
     }
 }
